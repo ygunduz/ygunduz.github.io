bb3d47cbfd1b77df2fd000034a44982c9d90d2ed
Yusuf Gündüz <yusuf.gunduz11@hacettepe.edu.tr>
2023-04-28 16:22:31 +0300

    Update capture.js

--- capture.js
+++ capture.js
@@ -33,7 +33,7 @@
       video: {
         width: { min: 1024, ideal: 1280, max: 1920 },
         height: { min: 576, ideal: 720, max: 1080 },
-        // facingMode: { exact: "environment" },
+        facingMode: { exact: "environment" },
       }, audio: false
     })
       .then(function (stream) {
